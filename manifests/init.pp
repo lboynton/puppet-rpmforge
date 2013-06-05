4042c3f604ccf472a3c0de44f4b6d0d7e5115915
Lee Boynton <lee@lboynton.com>
2013-06-05 17:00:12 +0100

    Remove trailing space

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class rpmforge($testing = 0) {
         gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag',
         descr       => "Rpmforge"
     }
-    
+
     yumrepo { 'rpmforge-extras':
         baseurl     => "http://apt.sw.be/redhat/el${::os_maj_version}/en/${::architecture}/extras/",
         enabled     => $testing,
@@ -14,7 +14,7 @@ class rpmforge($testing = 0) {
         gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag',
         descr       => "Rpmforge extras"
     }
-    
+
     file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag":
         ensure => present,
         owner  => 'root',
@@ -22,7 +22,7 @@ class rpmforge($testing = 0) {
         mode   => '0644',
         source => "puppet:///modules/rpmforge/RPM-GPG-KEY-rpmforge-dag",
     }
-    
+
     epel::rpm_gpg_key{ "rpmforge-dag":
         path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag",
     }
